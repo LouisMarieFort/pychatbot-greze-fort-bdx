7d20bbce319ac5726d8d77001544d54071b72228
LouisMarieFort <louis-marie.fort@efrei.net>
2023-11-08 21:06:06 +0100

    fonction remove_file_punctuation()

--- fonctions.py
+++ fonctions.py
@@ -15,10 +15,10 @@ def authors_name(fileName: str):
         beginningIndex -= 1
     return fileName[beginningIndex : endIndex]
 
-def president_first_name(secondName: str):
+def president_first_name(lastName: str):
     presidents = {"Chirac": "Jacques", "Giscard d'Estaing": "Valéry", "Mitterrand": "François", "Macron": "Emmanuel", "Sarkozy": "Nicolas"}
-    if secondName in presidents.keys():
-        return presidents[secondName]
+    if lastName in presidents.keys():
+        return presidents[lastName]
     print("Ce président n'est pas enregistré.")
     return None
 
@@ -28,7 +28,27 @@ def president_list_display():
     return None
 
 def create_cleaned_file(fileName: str):
-    file = open("./speeches/" + fileName, 'r', encoding='UTF-8')
-    cleanedFile = open("./cleaned/" + fileName, 'w', encoding='UTF-8')
+    file = open("./speeches/" + fileName, 'r', encoding = 'UTF-8')
+    cleanedFile = open("./cleaned/" + fileName, 'w', encoding = 'UTF-8')
     for line in file.readlines():
         cleanedFile.write(line.lower())
+
+def remove_file_punctuation(fileName: str):
+    punctuation = {'.', '\n', ',', '?', ';'}
+    specificPunctuation = {'-', "'"}
+    file = open("./cleaned/" + fileName, 'r', encoding = 'UTF-8')
+    text = file.read()
+    for punctuationMark in punctuation:
+        text = text.replace(punctuationMark, '')
+    for punctuationMark in specificPunctuation:
+        text = text.replace(punctuationMark, ' ')
+    file.close()
+    file = open("./cleaned/" + fileName, 'w', encoding = 'UTF-8')
+    file.write(text)
+    file.close()
+
+create_cleaned_file("Nomination_Chirac1.txt")
+remove_file_punctuation("Nomination_Chirac1.txt")
+text = "a,;bd"
+text = text.replace(',', 'A')
+print(text)
