f7e3ffd500acf68082f7d77bd06dd52f6b26b6dd
LouisMarieFort <louis-marie.fort@efrei.net>
2023-11-24 22:25:38 +0100

    fonction allAuthorsSaid terminée

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from fonctions import *
 
 # Test rapide des fonctions
 for file in listdir("./speeches/"):
-    create_cleaned_file(file)
+    createCleanedFile(file)
     remove_file_punctuation(file)
 #print(inverse_document_frequency())
 #print(TFIDF_matrix())
-#print(createUselessWordsList())
+print(createUselessWordsList())
 #print(createHigherTfidfWordsList())
 #print(mostRepeatedWords())
-print(list(findAuthorsWhoMentioned("Nation").keys()))
-print(findAuthorsWhoMostRepeated("Nation"))
+#print(list(findAuthorsWhoMentioned("Nation").keys()))
+#print(findAuthorsWhoMostRepeated("Nation"))
+#print(createWhoseTextIsIt())
+print(allAuthorsSaid())
