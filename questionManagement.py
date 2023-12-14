e2473da9e213eb94e2fac450982a6f4559aaad44
LouisMarieFort <louis-marie.fort@efrei.net>
2023-12-14 11:06:09 +0100

    4 de la partie II

--- questionManagement.py
+++ questionManagement.py
@@ -1,6 +1,7 @@
 from re import sub
 from tfidfFunctions import *
 from os import listdir
+from math import sqrt
 
 def getLoweredString(string : str) -> str:
     return string.lower()
@@ -27,64 +28,20 @@ def getIntersectionWords(wordsList : list, directory = "./cleaned/") -> list:
             intersection.append(word)
     return intersection
 
-def getQuestionTf(text : str, intersection : list) -> dict:
-    """ Create a dictionary of the term frequency of each word
-    Argument :
-        text : the text of which we want the term frequency
-               it has to be "clean" : in lowercase characters with no punctuation
-    Return :
-        dictionary : dictionary associating each word with its number of occurrences in the text
-    """
-    dictionary = dict()
-    endIndex = len(text)
-    while endIndex > 0:
-        beginningIndex = endIndex
-        while text[beginningIndex - 1] != ' ' and beginningIndex > 0:
-            beginningIndex -= 1
-        if text[beginningIndex : endIndex] not in dictionary.keys():
-            dictionary[text[beginningIndex : endIndex]] = 0
-        endIndex = beginningIndex - 1
-    for word in intersection:
-        if word in dictionary.keys():
-            dictionary[word] += 1
-    return dictionary
+def getDotProduct(vector1 : list, vector2 : list) -> float:
+    dotProduct = 0
+    for i in range(len(vector1)):
+        dotProduct += vector1[i] * vector2[i]
+    return dotProduct
 
-def getQuestionTfidfMatrix(intersection : list, directory = "./cleaned/") -> list:
-    """ Create a matrix of the TF-IDF of the corpus of documents
-        Each row represents the TF_IDF of the word that is at index 0
-        Then, each column corresponds to a different text
-    Argument :
-        directory (optional) : the directory that contains the corpus of cleaned documents
-    Return :
-        matrix : the TF-IDF matrix
-    """
-    matrix = []
-    filesNamesList = listdir(directory)
-    listOfTF = []                                                            #list of dictionaries
-    for fileName in filesNamesList:
-        with open(directory + fileName, 'r', encoding = "UTF-8") as file:
-            listOfTF.append(getQuestionTf(file.read(), intersection))
-    for wordAndIDF in inverseDocumentFrequency(directory).items():
-        row = [wordAndIDF[0]]
-        for column in range(len(filesNamesList)):
-            if wordAndIDF[0] in listOfTF[column].keys():
-                row.append(wordAndIDF[1] * listOfTF[column][wordAndIDF[0]])
-            else:
-                row.append(0)
-        matrix.append(row)
-    return matrix
+def getVectorNorm(vector : list) -> float:
+    sum = 0
+    for i in range(len(vector)):
+        sum += vector[i] ** 2
+    return sqrt(sum)
 
-def getTfidfVectorOfDocument(fileName : str, tfidfMatrix : list, directory = "./cleaned/") -> list:
-    vector = list()
-    fileIndex = 0
-    isFound = None
-    while fileIndex <= len(listdir(directory)) and not isFound:
-        if fileName == listdir(directory)[fileIndex]:
-            isFound = True
-        fileIndex += 1                        # On conserve la valeur supérieure puisque les indices de notre matrice TF-IDF sont de 1 de plus
-    for row in tfidfMatrix:
-        vector.append(row[fileIndex])
-    return vector
+def getCosineSimilarity(vector1 : list, vector2 : list) -> float:
+    return getDotProduct(vector1, vector2) / (getVectorNorm(vector1) * getVectorNorm(vector2))
 
 #tests
 cleanedQuestion = getCleanedQuestion("Qui a parlé en premier du climat ? officiellement grng zebi")
@@ -92,4 +49,8 @@ intersection = getIntersectionWords(cleanedQuestion)
 text = open("./cleaned/Nomination_Chirac1.txt", 'r', encoding = "UTF-8").read()
 #print(getQuestionTf(text, intersection))
 #print(getQuestionTfidfMatrix(intersection))
-print(getTfidfVectorOfDocument("Nomination_Sarkozy.txt", createTfidfMatrix()))
+#print(intersection)
+#print(getQuestionTf(intersection))
+#print(getQuestionTfidfVector(getQuestionTf(intersection)))
+#print(getDotProduct(getTfidfVectorOfDocument("Nomination_Chirac2.txt", createTfidfMatrix()), getQuestionTfidfVector(getQuestionTf(intersection))))
+print(getCosineSimilarity([0,2],[0,2]))
